1178525e94d9992863f3e412e01cd76e76127714
ermallimaj <ermallimaj32@gmail.com>
2023-05-09 11:02:18 +0200

    Imports needed for the code

---
+++ Projekt/3DES_2Keys.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import filedialog
+from Crypto.Cipher import DES3
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+import binascii
+
+
+
+
+
