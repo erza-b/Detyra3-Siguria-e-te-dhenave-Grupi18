52d938c69459cc5b27ff32f2aa624db8eb7ef2f2
erzamerovci <erzaamerovci@gmail.com>
2023-05-14 11:44:15 +0200

    addedFunctions_convertion

---
+++ Projekt/otherfiles.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import filedialog
+from Crypto.Cipher import DES3
+from Crypto.Util.Padding import pad,unpad
+from Crypto.Random import get_random_bytes
+import binascii
+
+def generate_random_key(size):
+    return get_random_bytes(size)
+
+def bytes_to_hex_string(data):
+    return binascii.hexlify(data).decode()
+
+def hex_string_to_bytes(hex_string):
+    return binascii.unhexlify(hex_string)
+
+def show_success_message(message):
+    success_label.config(text=message)
