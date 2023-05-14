1b7216cf3d2ac1b7241d6fbcd8c0fd69de827a5e
erzamerovci <erzaamerovci@gmail.com>
2023-05-14 11:52:40 +0200

    added_encryption_function

--- Projekt/otherfiles.py
+++ Projekt/otherfiles.py
@@ -16,3 +16,25 @@ def hex_string_to_bytes(hex_string):
 
 def show_success_message(message):
     success_label.config(text=message)
+
+def encrypt_decrypt_file():
+    if file_path:
+        try:
+            key1=generate_random_key(8)
+            key2=generate_random_key(8)
+            iv=get_random_bytes(8)
+
+            with open(file_path,"rb") as file:
+                data=file.read()
+            encrypt_cipher=DES3.new(key1+key2,DES3.MODE_CBC,iv)
+            encrypted_data=encrypt_cipher.encrypt(pad(data,DES3.block_size))
+            encrypted_hex=bytes_to_hex_string(encrypted_data)
+            encrypted_file_path=filedialog.asksaveasfilename(defaultextension=".enc")
+                if encrypted_file_path:
+                    with open(encrypted_file_path,"w")
+            as encrypted_file:
+        encrypted_file.write(encrypted_hex)
+            show_success_message("Enkriptimi u krye me sukses!")
+            print("File i enkriptuar u ruajt ne :",encrypted_file_path)
+
+        
